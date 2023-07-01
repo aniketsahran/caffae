fb9eaf5954abfb11e34b836db37a1b4e4838b3fd
ved-ant <vedantgadodia@gmail.com>
2023-07-01 17:55:44 +0530

    header changed

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,19 +2,25 @@ import React from 'react';
 import './Header.css';
 import image2 from '../../assets/group20.png';
 import image1 from '../../assets/ellipse1.png';
+import image3 from '../../assets/caffaeraw-031.png';
+import image4 from '../../assets/caffaeraw-041.png';
 const Header = () => {
   return (
     <div className="header-container">
     <div className='left_desc-text'>
     <div className='desc_text-container'>
+      <div className='header-logo'>
+    <img src={image4} className='logo-top' />  
+    <img src={image3} className='caffae-top' />
+      </div>
       <div className='header-title'>
       Discover, Learn, Connect
       The <span className='other'>caffae</span> way! 
       </div>
-      <br></br>
       <div className='header-desc'>
       Caffae is a transformative platform connecting seekers with experienced Gurus. Gain valuable advice, personalized guidance, and inspiration from those who have triumphed on the same path. Tap into a wealth of firsthand experiences, practical advice, and tailored guidance from our esteemed Gurus.Let Caffae bridge the gap between aspiration and achievement. Join us today!
       </div>
+      <button className='header-button'>Join our community</button>
     </div>
     </div>
     <div className='left_desc-space'></div>
