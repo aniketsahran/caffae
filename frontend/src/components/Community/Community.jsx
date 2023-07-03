9395515f981a5c08946b4fd7514acc7bf86592a3
Aniket <aniketsahran3@gmail.com>
2023-07-03 22:16:59 +0530

    Responsiveness done for now

--- frontend/src/components/Community/Community.jsx
+++ frontend/src/components/Community/Community.jsx
@@ -14,11 +14,13 @@ const Community = () => {
                 <div className='community-right'>
                     <div className='community-right-heading'>Having doubts and questions about college counselling?</div>
                     <div className='community-right-subheading'>Embark on a transformative journey of college counseling as our gurus illuminate your path to success. Discover personalized guidance, expert insights, and a supportive community eager to shape your future.</div>
+                    <div className='button-space'></div>
                     <a href="https://www.google.com/" target='_blank'><button className='community-button'><img src={image2} className='button-image' />Join the group</button></a>
                 </div>
                 <div className='community-right second'>
                     <div className='community-right-heading'>Want to Know what skills to Explore before joining college?</div>
                     <div className='community-right-subheading'>Embark on a knowledge expedition with our expert gurus as they unveil a world of skills waiting to be explored. Join Us and ignite your curiosity, discover new talents, and unlock your full potential. </div>
+                    <div className='button-space'></div>
                     <a href="https://www.google.com/" target='_blank'><button className='community-button'><img src={image2} className='button-image' />Join the group</button></a>
                 </div>
             </div>
